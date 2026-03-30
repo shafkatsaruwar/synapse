ad41df2f7690789b7e31764f1af2b7689fd30453
Shafkat Saruwar <mohammedsaruwar@icloud.com>
2026-03-30 13:10:32 -0400

    Bump version to 1.9.7 build 1.1

--- synapse-reset/app.config.js
+++ synapse-reset/app.config.js
@@ -19,7 +19,7 @@ module.exports = {
   expo: {
     name: "Synapse",
     slug: "synapse",
-    version: "1.9.6",
+    version: "1.9.7",
     orientation: "default",
     icon: "./assets/icon.png",
     scheme: "myapp",
@@ -32,7 +32,7 @@ module.exports = {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.mohammedsaruwar.synapse",
-      buildNumber: "12.3",
+      buildNumber: "1.1",
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
         NSFaceIDUsageDescription: "Unlock Synapse with Face ID to keep your health data private.",
